5f34202bf61ec58203f47b62ae48072184590e1f
trentnewman <tacnewman@gmail.com>
2019-08-01 17:52:58 -0700

    basic structure

---
+++ 03_scripts/script.py
@@ -0,0 +1,6 @@
+import pandas
+df = pandas.read_csv('../02_input/input.csv')
+print(df)
+out = df.groupby(['date'])['a'].agg(['mean','std','count'])
+out = out.reset_index()
+out.to_csv('../04_output/output.csv', index=False)
